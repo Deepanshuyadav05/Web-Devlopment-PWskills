332c724ce4961db096340a8d46f92f13f6498591
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-08-10 08:14:30 +0530

    Data type

---
+++ JavaScript/Module-27/Data_Type.js
@@ -0,0 +1,9 @@
+let name="deep";
+let age=18;
+let alive=true;
+let state;
+let money=null;
+console.log([typeof name,typeof age, typeof alive, typeof state, typeof money]);
+console.log(typeof null);                  //the null is stand-alone value and its type is object  //null is object
+console.log(typeof undefined);             //the type of undefined is undefined //undefined is itself a type
+
