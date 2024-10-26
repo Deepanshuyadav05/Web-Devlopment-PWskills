4d7e70a48112f28aa26991fd7d319de929f6ef58
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-10-26 11:41:40 +0530

    JSX

---
+++ React/Tic-Tac-Toe/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Grid from './Components/Grid/Grid'
+
+function App() {
+
+  return (
+    <div className="mains">
+        <Grid numberOfCards = {9}/>
+    </div>
+  )
+}
+
+export default App
